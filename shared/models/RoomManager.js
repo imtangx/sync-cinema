da9993da261d42929dfaa079d091d63d3d40316d
imtangx <939174106@qq.com>
2025-01-11 21:57:57 +0800

    feat(RoomManager): 新增首页websocket连接处理

--- shared/models/RoomManager.js
+++ shared/models/RoomManager.js
@@ -1,10 +1,42 @@
-import { RoomState } from './RoomState.js';
-import WebSocket from 'ws';
+import { RoomState } from "./RoomState.js";
+import { MessageType } from "../constants.js";
+import { Message } from "./Message.js";
+import WebSocket from "ws";
 
 export class RoomManager {
   constructor() {
     this.rooms = new Map(); // roomId -> RoomState
     this.userConnections = new Map(); // username -> websocket
+    this.homeConnections = new Set(); // homepage -> websocket
+  }
+
+  addHomeConnection(ws) {
+    this.homeConnections.add(ws);
+  }
+
+  removeHomeConnection(ws) {
+    this.homeConnections.delete(ws);
+  }
+
+  broadcastToHome() {
+    const onlineRooms = [];
+    this.rooms.forEach((room, roomId) => {
+      if (room.onlineUsers.length > 0) {
+        onlineRooms.push({
+          roomId,
+          userCount: room.onlineUsers.length
+        });
+      }
+    });
+
+    this.homeConnections.forEach((ws) => {
+      if (ws.readyState === WebSocket.OPEN) {
+        const message = new Message(MessageType.ONLINE_ROOMS_UPDATE, { onlineRooms });
+        ws.send(JSON.stringify(message));
+      } else {
+        this.removeHomeConnection(ws);
+      }
+    });
   }
 
   getRoom(roomId) {
@@ -23,22 +55,24 @@ export class RoomManager {
     const room = this.getRoom(roomId);
     room.onlineUsers.push(username);
     this.userConnections.set(username, ws);
+    this.broadcastToHome();
     return room;
   }
 
   removeUserFromRoom(username, roomId) {
     const room = this.getRoom(roomId);
-    room.onlineUsers = room.onlineUsers.filter(user => user !== username);
+    room.onlineUsers = room.onlineUsers.filter((user) => user !== username);
     this.userConnections.delete(username);
-
     if (room.onlineUsers.length === 0) {
       this.rooms.delete(roomId);
     }
+
+    this.broadcastToHome();
   }
 
   broadcast(roomId, message) {
     const room = this.getRoom(roomId);
-    room.onlineUsers.forEach(username => {
+    room.onlineUsers.forEach((username) => {
       const ws = this.userConnections.get(username);
       if (ws && ws.readyState === WebSocket.OPEN) {
         ws.send(JSON.stringify(message));
@@ -46,5 +80,3 @@ export class RoomManager {
     });
   }
 }
-
-
