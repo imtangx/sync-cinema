46aa65cdcc759e29b3dec85a8364df6e981b9f3d
imtangx <939174106@qq.com>
2025-01-11 21:53:21 +0800

    chore(App): 更改路由页面组件

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import EnterRoomCard from "./components/EnterRoomCard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 import RoomPage from "./pages/RoomPage.jsx";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <Router basename="/sync-cinema">
         <Routes>
-          <Route path="/" element={<EnterRoomCard />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/room/:id" element={<RoomPage />} />
         </Routes>
       </Router>
